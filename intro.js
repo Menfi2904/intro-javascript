ea4a0c9ede32399de68998f4bd0414810aec023e
Menfi Garcia <menfigarcia31@gmail.com>
2023-08-09 21:14:45 -0600

    primeros comandos en js

---
+++ intro.js
@@ -0,0 +1,25 @@
+let variable = 3
+let string = "Menfi"
+let x = "3"
+let boolean = true
+
+
+
+let array = [8, "menfi", false, 'p', 4.0, 8, "menfi", false, 'p', 4.0, 8, "menfi", false, 'p', 4.0, "tiene nota 5"];
+
+console.log(variable)
+console.log(string)
+console.log(x)
+console.log(boolean)
+console.log(array.length)
+console.log(array[array.length-1])
+
+let objeto = {
+    numero: 1,
+    nombre: "menfi",
+    boolean: false,
+    array: [1,2,3],
+
+}
+
+console.log(objeto.boolean)
